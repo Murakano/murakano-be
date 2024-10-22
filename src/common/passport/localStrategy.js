d02476d8f50c0ea4f5140b90c9c3f4a357243427
jjikky <kok5417@skuniv.ac.kr>
2024-10-22 21:51:04 +0900

    ♻️ passport 로직 메서드 추출로 가독성 향상

--- src/common/passport/localStrategy.js
+++ src/common/passport/localStrategy.js
@@ -9,22 +9,30 @@ module.exports = () => {
                 usernameField: 'email',
                 passwordField: 'password',
             },
-            async (email, password, done) => {
-                try {
-                    const user = await User.findOne({ email });
-                    if (!user) {
-                        return done(null, false, { message: '가입되지 않은 회원입니다.' });
-                    }
-                    const isMatch = await user.comparePassword(password);
-                    if (!isMatch) {
-                        return done(null, false, { message: '비밀번호가 일치하지 않습니다.' });
-                    }
-                    return done(null, user);
-                } catch (error) {
-                    console.error(error);
-                    return done(error);
-                }
-            }
+            authenticateUser
         )
     );
+};
+
+const authenticateUser = async (email, password, done) => {
+    try {
+        const user = await findUserByEmail(email);
+        if (!user) {
+            return done(null, false, { message: '가입되지 않은 회원입니다.' });
+        }
+
+        const isPasswordMatch = await user.comparePassword(password);
+        if (!isPasswordMatch) {
+            return done(null, false, { message: '비밀번호가 일치하지 않습니다.' });
+        }
+
+        return done(null, user);
+    } catch (error) {
+        console.error(error);
+        return done(error);
+    }
+};
+
+const findUserByEmail = async (email) => {
+    return await User.findOne({ email });
 };
